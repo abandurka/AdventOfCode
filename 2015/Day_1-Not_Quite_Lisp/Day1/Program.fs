32bdc5732b4ff5ad3503c202e084965ad56437ba
abandurka <abandurka@ptsecurity.com>
2024-12-04 18:55:34 +0300

    added sln

---
+++ 2015/Day_1-Not_Quite_Lisp/Day1/Program.fs
@@ -0,0 +1,20 @@
+﻿open System.IO
+
+let filepath = "..\..\..\..\input.txt"
+let lines = File.ReadAllText(filepath)
+
+let mapBracets = 
+    function
+    | '(' -> 1
+    | ')' -> -1
+    | _ -> 0
+    
+let myModifiedList = [for i in lines do yield  mapBracets i ]
+
+let res = 
+    myModifiedList
+    |> Seq.sum
+
+printfn "result is %d" res
+
+//138
