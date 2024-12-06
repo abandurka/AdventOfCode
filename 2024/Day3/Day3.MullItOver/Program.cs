f8c7182abc139895561f00a8a9d04f29d5e6de40
abandurka <abandurka@ptsecurity.com>
2024-12-06 11:29:51 +0300

    day3-2

--- 2024/Day3/Day3.MullItOver/Program.cs
+++ 2024/Day3/Day3.MullItOver/Program.cs
@@ -11,10 +11,42 @@ class Program
 
         var regex = new Regex("mul\\((\\d+),(\\d+)\\)");
         var matches = lines
-            .Select(x=> regex.Matches(x))
-            .SelectMany(f=> f)
-            .Select(x=> Mul(x.Groups[1].Value, x.Groups[2].Value))
+            .Select(x => regex.Matches(x))
+            .SelectMany(f => f)
+            .Select(x => Mul(x.Groups[1].Value, x.Groups[2].Value))
             .Sum();
+
+        Console.WriteLine(matches);
+
+        var regex2 = new Regex("mul\\((\\d+),(\\d+)\\)|do\\(\\)|don't\\(\\)");
+        var matches2 = lines
+            .Select(x => regex2.Matches(x))
+            .SelectMany(f => f)
+            ;
+
+        var sum = 0;
+        var enabled = true;
+        foreach (var x in matches2)
+        {
+            switch (x.Groups[0].Value)
+            {
+                case "do()":
+                    enabled = true;
+                    continue;
+                case "don't()":
+                    enabled = false;
+                    continue;
+                
+            }
+
+            if (enabled && x.Groups[0].Value.StartsWith("mul"))
+            {
+                sum += Mul(x.Groups[1].Value, x.Groups[2].Value);
+            }
+        }
+        
+
+        Console.WriteLine($"sum2: {sum}");
     }
 
     private static int Mul(string l, string r)
